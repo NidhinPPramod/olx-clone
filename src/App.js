8d17d23e9a9c9eff2f1993a642fe740b4d7c3ca1
Nidhin P Pramod <kichum386@gamil.com>
2022-06-12 19:18:50 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
-
-/**
- * ?  =====Import Components=====
- */
 import Home from './Pages/Home';
+import Signup from "./Pages/Signup";
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Route path="/" component={Home}/>
+                <Route path="/signup" component={Signup}/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
